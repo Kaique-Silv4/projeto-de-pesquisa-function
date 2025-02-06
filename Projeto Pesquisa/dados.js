46ec6ee5a9f0cd922886adecc6a966409762bc97
Kaique Silva <kaka.s.ribeiro46@gmail.com>
2025-02-06 18:27:09 -0300

    Add files via upload

---
+++ Projeto Pesquisa/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Nissan Skyline R34",
+        descricao: "O carro icônico de Velozes e Furiosos. Dirigido por Bryan O'Conner, R34 um dos carros queridinhos dos fãs.",
+        link: "https://pt.wikipedia.org/wiki/Nissan_Skyline_GTR_R34",
+        tags: "R34 skyline"
+    },
+    {
+        titulo: "Dodge Charger",
+        descricao: "O lendário Dodge Charger de 1970 de Dominic Toretto, de Velozes e Furiosos, é um muscle car preto, equipado com um motor V8 supercharged de 900 cv. Conhecido por seu torque brutal e velocidade impressionante, é o símbolo da força e legado da família Toretto, destacando-se em corridas de arrancada e cenas de ação icônicas.",
+        link: "https://en-m-wikipedia-org.translate.goog/wiki/Dodge_Charger?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc",
+        tags: "Carro Toretto"
+    },
+    {
+        titulo: "Toyota Supra",
+        descricao: "Laranja e icônico, o Toyota Supra de 1995 de Brian O'Conner tem motor 2JZ-GTE turbo, preparado para alta performance. Famoso por sua velocidade e estilo, é destaque nas corridas de rua da franquia.",
+        link: "https://pt.wikipedia.org/wiki/Toyota_Supra#Quarta_gera%C3%A7%C3%A3o_(A80;_1993-2002)",
+        tags: "Supra Bryan"
+    }
+];
